719268458027fcbdb92a96021a12b08650203670
DilmiCaldera <74985257+DilmiCaldera@users.noreply.github.com>
2021-07-03 02:13:00 +0530

    colour detection completed

---
+++ Pillar_counting/MY_ROBO/controllers/ColorDetection/build/release/ColorDetection.d
@@ -0,0 +1,8 @@
+build/release/ColorDetection.o: ColorDetection.cpp \
+ F:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ F:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ F:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ F:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ F:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ F:\Program\ Files\Webots/include/controller/cpp/webots/Camera.hpp \
+ F:/Program\ Files/Webots/include/controller/c/webots/camera_recognition_object.h
--- Pillar_counting/MY_ROBO/controllers/obstacle_avoider/build/release/obstacle_avoider.d
+++
@@ -1,6 +0,0 @@
-build/release/obstacle_avoider.o: obstacle_avoider.cpp \
- F:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- F:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- F:/Program\ Files/Webots/include/controller/c/webots/types.h \
- F:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
- F:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp
