02f6183ef325b199de09f8816dcecca2e89930cd
DilmiCaldera <74985257+DilmiCaldera@users.noreply.github.com>
2021-07-03 02:18:27 +0530

    colour detection uploaded

---
+++ Pillar_counting/MY_ROBO/controllers/obstacle_avoider/build/release/obstacle_avoider.d
@@ -0,0 +1,6 @@
+build/release/obstacle_avoider.o: obstacle_avoider.cpp \
+ F:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ F:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ F:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ F:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ F:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp
