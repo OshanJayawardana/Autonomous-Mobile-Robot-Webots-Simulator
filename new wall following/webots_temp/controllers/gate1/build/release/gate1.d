fee07da520b5e346d8a04f9b72315116c95b83c4
ArunaLakpriya <78334451+ArunaLakpriya@users.noreply.github.com>
2021-07-28 10:00:26 +0530

    new wall follwing

---
+++ new wall following/webots_temp/controllers/gate1/build/release/gate1.d
@@ -0,0 +1,6 @@
+build/release/gate1.o: gate1.cpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ D:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp
